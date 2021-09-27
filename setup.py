38cd0638b66fd98292562ff709ea5cf448c986bc
Fritz Davenport <fritz@astronomer.io>
2021-09-27 13:15:32 -0400

    spike checkin

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+import pathlib
+
+from pkg_resources import parse_requirements
+from setuptools import setup, find_packages
+
+with pathlib.Path('requirements.txt').open() as requirements_txt:
+    install_requires = [str(requirement) for requirement in parse_requirements(requirements_txt)]
+
+VERSION = '0.0.1'
+repo = 'fritz-astronomer/orrery'
+
+setup(
+    name='orrery',
+    version=VERSION,
+    description='',
+    long_description=open("README.md").read(),
+    url=f'git@github.com:{repo}.git',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=install_requires,
+    # entry_points={'console_scripts': ['orrery=orrery.main:main']}
+)
