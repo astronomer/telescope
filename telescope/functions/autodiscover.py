d8ab6b54ef6cef15ba5ab27417d64f196dad0f5d
Fritz Davenport <fritz@astronomer.io>
2021-11-16 18:17:37 -0500

    add guard against uninitialized docker/kube client

--- telescope/functions/autodiscover.py
+++ telescope/functions/autodiscover.py
@@ -5,18 +5,24 @@ from telescope.getters.kubernetes import KubernetesGetter
 
 
 def docker_autodiscover(**kwargs) -> List[Dict[str, str]]:
-    return [
-        {"container_id": container.short_id}
-        for container in LocalDockerGetter.docker_client.containers.list(filters={"name": "scheduler"})
-    ]
+    if LocalDockerGetter.docker_client is not None:
+        return [
+            {"container_id": container.short_id}
+            for container in LocalDockerGetter.docker_client.containers.list(filters={"name": "scheduler"})
+        ]
+    else:
+        return []
 
 
 def kube_autodiscover(label_selector, **kwargs) -> List[Dict[str, str]]:
     """:returns List of Tuple containing - pod name, pod namespace, container name"""
-    return [
-        {"name": r.metadata.name, "namespace": r.metadata.namespace, "container": "scheduler"}
-        for r in KubernetesGetter.kube_client.list_pod_for_all_namespaces(label_selector=label_selector).items
-    ]
+    if KubernetesGetter.kube_client is not None:
+        return [
+            {"name": r.metadata.name, "namespace": r.metadata.namespace, "container": "scheduler"}
+            for r in KubernetesGetter.kube_client.list_pod_for_all_namespaces(label_selector=label_selector).items
+        ]
+    else:
+        return []
 
 
 AUTODISCOVERERS = {
