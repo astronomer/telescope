6f7d30cc4be2058fb46a56e00cc5c13a307d2f4b
fritz-astronomer <fritz@astronomer.io>
2022-03-25 21:59:37 -0400

    1.10.2+2.2.4 support - manual testing

--- airflow_report/__main__.py
+++ airflow_report/__main__.py
@@ -7,6 +7,7 @@ import re
 import sys
 from functools import reduce
 
+import airflow.version
 from sqlalchemy import text
 
 logging.getLogger("airflow.settings").setLevel(logging.ERROR)
@@ -159,77 +160,83 @@ def pools_report() -> Any:
     try:
         return Pool.slots_stats()
     except AttributeError:
-        from typing import Dict, Iterable, Optional, Tuple
+        try:
+            from typing import Dict, Iterable, Optional, Tuple
 
-        from airflow.exceptions import AirflowException
-        from airflow.models import Pool
-        from airflow.utils.state import State
-        from sqlalchemy.orm.session import Session
+            from airflow.exceptions import AirflowException
+            from airflow.models import Pool
+            from airflow.utils.state import State
+            from sqlalchemy.orm.session import Session
 
-        EXECUTION_STATES = {
-            State.RUNNING,
-            State.QUEUED,
-        }
+            EXECUTION_STATES = {
+                State.RUNNING,
+                State.QUEUED,
+            }
 
-        @provide_session
-        def slots_stats(
-            *,
-            lock_rows: bool = False,
-            session: Session = None,
-        ) -> Dict[str, dict]:
-            """
-            Get Pool stats (Number of Running, Queued, Open & Total tasks)
-            If ``lock_rows`` is True, and the database engine in use supports the ``NOWAIT`` syntax, then a
-            non-blocking lock will be attempted -- if the lock is not available then SQLAlchemy will throw an
-            OperationalError.
-            :param lock_rows: Should we attempt to obtain a row-level lock on all the Pool rows returns
-            :param session: SQLAlchemy ORM Session
-            """
-            from airflow.models.taskinstance import TaskInstance  # Avoid circular import
+            @provide_session
+            def slots_stats(
+                *,
+                lock_rows: bool = False,
+                session: Session = None,
+            ) -> Dict[str, dict]:
+                """
+                Get Pool stats (Number of Running, Queued, Open & Total tasks)
+                If ``lock_rows`` is True, and the database engine in use supports the ``NOWAIT`` syntax, then a
+                non-blocking lock will be attempted -- if the lock is not available then SQLAlchemy will throw an
+                OperationalError.
+                :param lock_rows: Should we attempt to obtain a row-level lock on all the Pool rows returns
+                :param session: SQLAlchemy ORM Session
+                """
+                from airflow.models.taskinstance import TaskInstance  # Avoid circular import
 
-            pools: Dict[str, dict] = {}
+                pools: Dict[str, dict] = {}
 
-            query = session.query(Pool.pool, Pool.slots)
+                query = session.query(Pool.pool, Pool.slots)
 
-            pool_rows: Iterable[Tuple[str, int]] = query.all()
-            for (pool_name, total_slots) in pool_rows:
-                if total_slots == -1:
-                    total_slots = float("inf")  # type: ignore
-                pools[pool_name] = dict(total=total_slots, running=0, queued=0, open=0)
+                pool_rows: Iterable[Tuple[str, int]] = query.all()
+                for (pool_name, total_slots) in pool_rows:
+                    if total_slots == -1:
+                        total_slots = float("inf")  # type: ignore
+                    pools[pool_name] = dict(total=total_slots, running=0, queued=0, open=0)
 
-            state_count_by_pool = (
-                session.query(TaskInstance.pool, TaskInstance.state).filter(
-                    TaskInstance.state.in_(list(EXECUTION_STATES))
-                )
-            ).all()
+                state_count_by_pool = (
+                    session.query(TaskInstance.pool, TaskInstance.state).filter(
+                        TaskInstance.state.in_(list(EXECUTION_STATES))
+                    )
+                ).all()
 
-            # calculate queued and running metrics
-            for (pool_name, state) in state_count_by_pool:
-                # Some databases return decimal.Decimal here.
-                count = 1
+                # calculate queued and running metrics
+                for (pool_name, state) in state_count_by_pool:
+                    # Some databases return decimal.Decimal here.
+                    count = 1
 
-                stats_dict = pools.get(pool_name)
-                if not stats_dict:
-                    continue
-                # TypedDict key must be a string literal, so we use if-statements to set value
-                if state == "running":
-                    stats_dict["running"] = count
-                elif state == "queued":
-                    stats_dict["queued"] = count
-                else:
-                    raise AirflowException(f"Unexpected state. Expected values: {EXECUTION_STATES}.")
+                    stats_dict = pools.get(pool_name)
+                    if not stats_dict:
+                        continue
+                    # TypedDict key must be a string literal, so we use if-statements to set value
+                    if state == "running":
+                        stats_dict["running"] = count
+                    elif state == "queued":
+                        stats_dict["queued"] = count
+                    else:
+                        raise AirflowException(f"Unexpected state. Expected values: {EXECUTION_STATES}.")
 
-            # calculate open metric
-            for pool_name, stats_dict in pools.items():
-                if stats_dict["total"] == -1:
-                    # -1 means infinite
-                    stats_dict["open"] = -1
-                else:
-                    stats_dict["open"] = stats_dict["total"] - stats_dict["running"] - stats_dict["queued"]
+                # calculate open metric
+                for pool_name, stats_dict in pools.items():
+                    if stats_dict["total"] == -1:
+                        # -1 means infinite
+                        stats_dict["open"] = -1
+                    else:
+                        stats_dict["open"] = stats_dict["total"] - stats_dict["running"] - stats_dict["queued"]
 
-            return pools
+                return pools
 
-        return slots_stats()
+            return slots_stats()
+        except ModuleNotFoundError as e:
+            # probably in airflow 1.10.2?
+            from airflow.api.common.experimental.pool import get_pools
+
+            return [p.to_json() for p in get_pools()]
 
 
 # noinspection SqlNoDataSourceInspection,PyBroadException
@@ -244,16 +251,28 @@ def dags_report(session) -> Any:
         [] if session.bind.dialect.name != "postgresql" else [literal_column("','")]
     )
 
-    dag_model_fields = [
-        DagModel.dag_id,
-        DagModel.schedule_interval,
-        DagModel.root_dag_id,
-        DagModel.is_paused,
-        DagModel.is_active,
-        DagModel.is_subdag,
-        DagModel.fileloc,
-        DagModel.owners,
-    ]
+    try:
+        dag_model_fields = [
+            DagModel.dag_id,
+            DagModel.schedule_interval,
+            DagModel.root_dag_id,
+            DagModel.is_paused,
+            DagModel.is_active,
+            DagModel.is_subdag,
+            DagModel.fileloc,
+            DagModel.owners,
+        ]
+    except AttributeError:
+        # some fields didn't exist in airflow 1.10.2
+        dag_model_fields = [
+            DagModel.dag_id,
+            DagModel.is_paused,
+            DagModel.is_active,
+            DagModel.is_subdag,
+            DagModel.fileloc,
+            DagModel.owners,
+        ]
+
     q = (
         session.query(
             *dag_model_fields,
@@ -327,15 +346,22 @@ def dag_complexity_report(dag_path: str):
 
 @provide_session
 def connections_report(session) -> List[str]:
-    from airflow.models.connection import Connection
+    try:
+        from airflow.models.connection import Connection
+    except ModuleNotFoundError:
+        # airflow 1.10.2 has a different import path
+        from airflow.models import Connection
 
     return [conn_id for (conn_id,) in session.query(Connection.conn_id)]
 
 
 @provide_session
 def variables_report(session) -> List[str]:
-    from airflow.models.variable import Variable
-
+    try:
+        from airflow.models.variable import Variable
+    except:
+        # airflow 1.10.2 has a different import path
+        from airflow.models import Variable
     return [key for (key,) in session.query(Variable.key)]
 
 
@@ -377,17 +403,23 @@ def usage_stats_report(session) -> Any:
 # noinspection SqlResolve
 @provide_session
 def user_report(session) -> Any:
+    from airflow.version import version
+    from sqlalchemy.exc import OperationalError
+
     dialect = session.bind.dialect.name
-    sql = text(
-        f"""
-        SELECT
-            (SELECT COUNT(id) FROM ab_user WHERE last_login > {days_ago(dialect, 1)}) AS "1_days_active_users",
-            (SELECT COUNT(id) FROM ab_user WHERE last_login > {days_ago(dialect, 7)}) AS "7_days_active_users",
-            (SELECT COUNT(id) FROM ab_user WHERE last_login > {days_ago(dialect, 30)}) AS "30_days_active_users",
-            (SELECT COUNT(id) FROM ab_user WHERE last_login > {days_ago(dialect, 365)}) AS "365_days_active_users",
-            (SELECT COUNT(id) FROM ab_user) AS "total_users";
-        """
-    )
+    if version >= "1.10.5":
+        sql = text(
+            f"""
+            SELECT
+                (SELECT COUNT(id) FROM ab_user WHERE last_login > {days_ago(dialect, 1)}) AS "1_days_active_users",
+                (SELECT COUNT(id) FROM ab_user WHERE last_login > {days_ago(dialect, 7)}) AS "7_days_active_users",
+                (SELECT COUNT(id) FROM ab_user WHERE last_login > {days_ago(dialect, 30)}) AS "30_days_active_users",
+                (SELECT COUNT(id) FROM ab_user WHERE last_login > {days_ago(dialect, 365)}) AS "365_days_active_users",
+                (SELECT COUNT(id) FROM ab_user) AS "total_users";
+            """
+        )
+    else:
+        sql = text("""SELECT COUNT(id) AS "total_users" FROM "users";""")
     for r in session.execute(sql):
         return dict(r)
     return {}
