55f1e6708551d40e0cdc99890495ef200e1a5e1d
fritz-astronomer <fritz@astronomer.io>
2022-05-10 22:35:54 -0400

    add obfuscation, log level, and downloading of current version release to slowtest

--- tests/resources/slowtest_dev.sh
+++ tests/resources/slowtest_dev.sh
@@ -4,11 +4,13 @@ rm -rf dev
 virtualenv dev
 source  dev/bin/activate
 set -x
-python -m pip install telescope poetry --find-links https://github.com/astronomer/telescope/releases/
+python -m pip install telescope poetry --find-links https://github.com/astronomer/telescope/releases/tag/v$(poetry version --short)
 
-TELESCOPE_REPORT_RELEASE_VERSION=$(poetry version --short) telescope --kubernetes --docker --local
+LOG_LEVEL=DEBUG TELESCOPE_REPORT_RELEASE_VERSION=$(poetry version --short) telescope --kubernetes --docker --local -n Astronomer
 
-TELESCOPE_KUBERNETES_METHOD=kubectl TELESCOPE_REPORT_RELEASE_VERSION=$(poetry version --short) telescope --kubernetes
+LOG_LEVEL=DEBUG TELESCOPE_KUBERNETES_METHOD=kubectl TELESCOPE_REPORT_RELEASE_VERSION=$(poetry version --short) telescope --kubernetes -n Astronomer
+
+LOG_LEVEL=DEBUG TELESCOPE_REPORT_RELEASE_VERSION=$(poetry version --short) telescope --kubernetes --dag-obfuscation -n Astronomer
 
 set +x
 deactivate
