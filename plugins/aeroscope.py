bdcaafa214bc3aa65cb5b01fc31b90c5296076bc
frankbreetz <frank.breetz@gmail.com>
2022-09-28 15:47:15 -0400

    merge branches

--- plugins/aeroscope.py
+++ plugins/aeroscope.py
@@ -11,6 +11,7 @@ import logging
 import socket
 from contextlib import redirect_stderr, redirect_stdout
 from json import JSONDecodeError
+
 import requests
 from airflow.configuration import conf
 from airflow.models.baseoperator import BaseOperator
